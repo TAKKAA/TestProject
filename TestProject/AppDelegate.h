2083048911302e099f4ea977ecf35e63be7cd8da
TAKKAA <tackn7@icloud.com>
2015-09-03 18:58:38 +0900

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by 中山　貴仁 on 2015/09/03.
+//  Copyright (c) 2015年 Nakayama Takahito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by 中山　貴仁 on 2015/09/03.
+//  Copyright (c) 2015年 Nakayama Takahito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
